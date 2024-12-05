cc931037bdebd066f0f78d4b1baee19e4f89aa32
heimin22 <fyketonel@gmail.com>
2024-12-05 08:36:03 +0800

    permissions

--- lib/NavigationPages/home_page.dart
+++ lib/NavigationPages/home_page.dart
@@ -1,12 +1,155 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(const HomeScreen());
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pasada',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFF2F2F2),
+        fontFamily: 'Inter',
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Pasada'),
+      routes: <String, WidgetBuilder>{
+
+      },
+    );
+  }
+}
+
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+  final String title;
+
   @override
   HomePageState createState() => HomePageState();
 }
 
 class HomePageState extends State<HomePage> {
   String _searchText = "";
+  late GoogleMapController mapController;
+  LocationData? _currentLocation;
+  late Location _location;
+
+  @override
+  void initState() {
+    super.initState();
+    _location = Location();
+    _checkPermissionsAndNavigate();
+  }
+
+  Future<void> _checkPermissionsAndNavigate() async {
+    try {
+      // check if location service is enabled
+      bool serviceEnabled = await _location.serviceEnabled();
+      if (!serviceEnabled) {
+        serviceEnabled = await _location.serviceEnabled();
+        if (!serviceEnabled) {
+          _showLocationServicesDialog();
+          return;
+        }
+      }
+      // check for and request location permissions
+      PermissionStatus permissionGranted = await _location.hasPermission();
+      if (permissionGranted == PermissionStatus.denied) {
+        permissionGranted = await _location.requestPermission();
+        if(permissionGranted != PermissionStatus.granted) {
+          _showPermissionDialog();
+          return;
+        }
+      }
+      // get current location
+      _currentLocation = await _location.getLocation();
+      if (_currentLocation != null) {
+        setState(() {});
+      } else {
+        _showLocationErrorDialog();
+      }
+    } catch (e) {
+      _showErrorDialog("An error occured while fetching the location.");
+    }
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  void _showPermissionDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Permission Required'),
+        content: Text(
+          'This app needs location permission to work. Please allow it in your settings.',
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('Ok'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showLocationServicesDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Enable Location Services'),
+        content: Text(
+          'Location services are disabled. Please enable them to use this feature.',
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showLocationErrorDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Location Error'),
+        content: Text('Unable to fetch the current location. Please try again later.'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Error'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
